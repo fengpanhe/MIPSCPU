7181f1164dfe395b3ef80a66ad33f1748cba88d1
he <ffpphh@outlook.com>
2017-09-29 16:06:46 +0800

    remove some dir

--- p_MIPS_CPU/p_MIPS_CPU.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xsim CPU_TEST2_behav -key {Behavioral:sim_1:Functional:CPU_TEST2} -tclbatch CPU_TEST2.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
