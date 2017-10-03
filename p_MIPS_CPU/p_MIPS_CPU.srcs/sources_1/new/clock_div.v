a128f6f6409531fb6eff691f8bacf21cb25c4968
he <ffpphh@outlook.com>
2017-10-04 00:39:08 +0800

    修改LED

---
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/clock_div.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/04/2017 12:33:30 AM
+// Design Name: 
+// Module Name: clock_div
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clock_div(
+    clk,
+    clk_sys_1HZ
+    );
+    input clk;
+    output clk_sys_1HZ;
+    
+    reg clk_sys_1HZ = 0;
+    reg [25:0] div_counter = 0;
+    always @(posedge clk)
+    begin
+        if(div_counter >= 50000000)
+        begin
+            clk_sys_1HZ <= ~clk_sys_1HZ;
+            div_counter = 0;
+        end else
+        begin
+            div_counter <= div_counter + 1;
+        end
+    end
+endmodule
---
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/dig_to_led.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2017 11:51:59 PM
+// Design Name: 
+// Module Name: dig_to_led_o
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dig_to_led(
+    input enable_i,
+    input[3:0] dig_i,
+    input point_i,
+    output reg[7:0] led_o
+    );
+
+    always @(*) begin
+        if (!enable_i) begin
+            led_o <= 8'hff;
+        end
+        else begin
+            case (dig_i)
+                4'h0: led_o[6:0] <= 7'hc0;
+                4'h1: led_o[6:0] <= 7'hf9;
+                4'h2: led_o[6:0] <= 7'ha4;
+                4'h3: led_o[6:0] <= 7'hb0;
+                4'h4: led_o[6:0] <= 7'h99;
+                4'h5: led_o[6:0] <= 7'h92;
+                4'h6: led_o[6:0] <= 7'h82;
+                4'h7: led_o[6:0] <= 7'hf8;
+                4'h8: led_o[6:0] <= 7'h80;
+                4'h9: led_o[6:0] <= 7'h90;
+                4'ha: led_o[6:0] <= 7'h88;
+                4'hb: led_o[6:0] <= 7'h83;
+                4'hc: led_o[6:0] <= 7'hc6;
+                4'hd: led_o[6:0] <= 7'ha1;
+                4'he: led_o[6:0] <= 7'h86;
+                4'hf: led_o[6:0] <= 7'h8e;
+                default: led_o[6:0] <= 7'hff;
+            endcase
+            led_o[7] <= point_i;
+        end    
+    end
+endmodule
