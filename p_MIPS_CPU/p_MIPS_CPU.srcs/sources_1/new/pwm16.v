3179a6687d9faa6990c03f848f1b401a2f79b416
he <ffpphh@outlook.com>
2017-10-03 22:53:01 +0800

    pwm和wtd

---
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/pwm16.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2017 10:20:14 PM
+// Design Name: 
+// Module Name: pwm16
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm16(
+    input reset,                    //系统复位
+    input[2:0] address,             //端口号
+    input[15:0] data,               //cpu写到pwm控制器的数据
+    input cs,                       //片选，接PWMCtrl
+    input clk,                      //系统时钟
+    input iow,                      //写信号
+    output reg pwm                 //输出方波信号
+    );
+
+    reg[15:0] maxcount;             //最大值寄存器
+    reg[15:0] midcount;             //对比寄存器
+    reg[15:0] flag;                 //使能寄存器
+    reg[15:0] rubbish;              //错误端口号的data处理
+    reg[15:0] counter;              //计数器
+
+
+    always @(negedge clk) begin
+        if (reset == 1) begin
+            // reset
+            maxcount = 16'hffff;
+            midcount = 16'h0000;
+            flag = 16'h0000;
+            pwm = 1;
+            counter = 16'h0000;
+        end
+        else if (cs == 1 && iow == 1) begin      //cpu写pwm寄存器
+            case (address[2:0])
+                3'b000: maxcount = data;
+                3'b010: midcount = data;
+                3'b100: flag[0] = data[0];      //写实能寄存器
+                default: rubbish = data;
+            endcase
+        end
+        else if (flag[0]) begin
+            if (counter == maxcount) begin      //计数到最大值
+                counter = 16'b0000;
+                pwm = 1;
+            end
+            else begin
+                counter = counter + 1'b1;
+                if (counter > midcount) begin       //计数值大于对比值
+                    pwm = 0;
+                end
+                else begin                          //计数值小于对比值
+                    pwm = 1;
+                end
+            end
+        end
+        else begin
+             pwm = 1;
+        end 
+    end
+endmodule
---
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/wtd16.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2017 10:38:24 PM
+// Design Name: 
+// Module Name: wtd16
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wtd16(
+    input reset,            //系统复位信号
+    input clk,              //系统时钟
+    input cs,               //片选。接wdtctrl
+    input iow,              //写信号
+    input[15:0] data,       //cpu写的数据
+    output reg rst          //输出的复位信号
+    );
+
+    reg[15:0] count;        //计数器
+    reg[2:0] cnt;           //小计数器，计4个时钟
+
+    always @(negedge clk) begin
+        if (reset == 1) begin
+            // reset
+            count = 16'hffff;
+            cnt = 3'b000;
+            rst = 0;
+        end
+        else begin
+            if (count == 16'd0) begin   //已计数到0
+                cnt = 3'b100;           //启动小计数器
+                count = 16'hffff;
+                rst = 1;                //输出复位信号
+            end
+            else begin
+                count = count - 1'b1;       //计数没到0，则计数器减1
+                if (cnt == 3'b000) begin     //小计数器到0，说明复位信号已经满4时钟周期
+                    rst = 0;
+                end
+                else begin
+                    cnt = cnt - 1'b1;       //小计数器不为0，减1
+                end
+
+                if ((cs == 1) && (iow == 1)) begin  //得到写信号后，看门狗全部复位
+                    count = 16'hffff;
+                    cnt = 3'b000;
+                    rst = 0;
+                end
+            end
+        end
+    end
+endmodule
