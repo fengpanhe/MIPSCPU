60fb935315e6465213f7d18e7594c85a1bed892f
he <ffpphh@outlook.com>
2017-10-15 00:53:59 +0800

    把键盘改为输出键值，并测试通过取键值的端口功能

--- p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/key16.v
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/key16.v
@@ -21,14 +21,14 @@
 
 
 module key16(
-    input wire reset,       //锟斤拷位锟脚猴拷
-    input wire cs,          //片选锟脚号ｏ拷锟斤拷keyctrl
-    input wire clk,         //系统时锟斤拷
-    input wire ior,         //锟斤拷锟脚猴拷
-    input wire[1:0] address,//锟剿口猴拷
-    input wire[3:0] col,    //锟斤拷锟斤拷
-    output reg[3:0] line,   //锟斤拷锟斤拷
-    output reg[15:0] ioread_data//锟斤拷锟斤拷锟斤拷锟�
+    input wire reset,       //閿熸枻鎷蜂綅閿熻剼鐚存嫹
+    input wire cs,          //鐗囬€夐敓鑴氬彿锝忔嫹閿熸枻鎷穔eyctrl
+    input wire clk,         //绯荤粺鏃堕敓鏂ゆ嫹
+    input wire ior,         //閿熸枻鎷烽敓鑴氱尨鎷�
+    input wire[1:0] address,//閿熷壙鍙ｇ尨鎷�
+    input wire[3:0] col,    //閿熸枻鎷烽敓鏂ゆ嫹
+    output reg[3:0] line,   //閿熸枻鎷烽敓鏂ゆ嫹
+    output reg[15:0] ioread_data//閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓锟�
     );
 
     reg[15:0] keyvalue;
@@ -45,7 +45,7 @@ module key16(
         else begin
             case(line)
                 4'b0000: if(col != 4'b1111) line <= 4'b1110;
-                4'b1110: //0锟斤拷
+                4'b1110: //0閿熸枻鎷�
                     if(col != 4'b1111) begin
                         case(col)
                             4'b1110: keyvalue = 16'h0001;
@@ -59,7 +59,7 @@ module key16(
                     else begin
                         line <= 4'b1101;
                     end
-                4'b1101: //1锟斤拷
+                4'b1101: //1閿熸枻鎷�
                     if(col != 4'b1111) begin
                         case(col)
                             4'b1110: keyvalue = 16'h0004;
@@ -73,7 +73,7 @@ module key16(
                     else begin
                         line <= 4'b1011;
                     end
-                4'b1011: //2锟斤拷
+                4'b1011: //2閿熸枻鎷�
                     if(col != 4'b1111) begin
                         case(col)
                             4'b1110: keyvalue = 16'h0007;
@@ -87,13 +87,13 @@ module key16(
                     else begin
                         line <= 4'b0111;
                     end
-                4'b0111: //3锟斤拷
+                4'b0111: //3閿熸枻鎷�
                     if(col != 4'b1111) begin
                         case(col)
                             4'b1110: keyvalue = 16'h0000;
                             4'b1101: keyvalue = 16'h000f;
                             4'b1011: keyvalue = 16'h000e;
-                            4'b0111: keyvalue = 16'h000dcd ;
+                            4'b0111: keyvalue = 16'h000d;
                         endcase
                         keystat = keystat | 16'h0001;
                         line <= 4'b0000;
@@ -104,11 +104,11 @@ module key16(
                     end
             endcase
 
-            if ((cs == 1) && (ior == 1)) begin // 锟斤拷锟脚猴拷
-                if (address == 2'b00) begin    //锟斤拷锟斤拷值
+            if ((cs == 1) && (ior == 1)) begin // 閿熸枻鎷烽敓鑴氱尨鎷�
+                if (address == 2'b00) begin    //閿熸枻鎷烽敓鏂ゆ嫹鍊�
                     ioread_data = keyvalue;
                 end
-                else if(address == 2'b10) begin //锟斤拷状态同时锟斤拷状态
+                else if(address == 2'b10) begin //閿熸枻鎷风姸鎬佸悓鏃堕敓鏂ゆ嫹鐘舵€�
                     ioread_data = keystat;
                     keystat = keystat & 16'hfffe;
                 end
