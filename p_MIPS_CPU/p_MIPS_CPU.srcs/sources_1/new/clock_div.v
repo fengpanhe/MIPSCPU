13709741fc495c36e1b50d96a1eceffc2fea6c05
he <ffpphh@outlook.com>
2017-10-17 23:46:51 +0800

    修正disp

--- p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/clock_div.v
+++ p_MIPS_CPU/p_MIPS_CPU.srcs/sources_1/new/clock_div.v
@@ -28,16 +28,15 @@ module clock_div(
     output clk_sys_1HZ;
     
     reg clk_sys_1HZ = 0;
-    reg [25:0] div_counter = 0;
+    reg [19:0] div_counter = 20'h186A0; //10^5d
     always @(posedge clk)
     begin
-        if(div_counter >= 5)
-        begin
+        if(div_counter == 0) begin
             clk_sys_1HZ <= ~clk_sys_1HZ;
-            div_counter = 0;
-        end else
-        begin
-            div_counter <= div_counter + 1;
+            div_counter = 20'h186A0;
+        end 
+        else begin
+            div_counter <= div_counter - 1;
         end
     end
 endmodule
